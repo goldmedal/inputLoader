5ae4a0b41bccfa2cea855f7960dfea2b6d54e040
Jax <liugs963@gmail.cm>
2015-07-10 18:53:43 +0800

    non-hdfs version finished

--- src/main/java/OGAInputLoader/OGAInputLoader/InputLoader.java
+++ src/main/java/OGAInputLoader/OGAInputLoader/InputLoader.java
@@ -14,16 +14,92 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class InputLoader {
 	
+
+	private static Configuration conf;
+	private static FileSystem hdfs;
+	private static String hostname = "node1";
+	
+	public static void set_hadoop() throws IOException
+	{
+		conf = new Configuration();
+    	hdfs = FileSystem.get(conf);
+	}
+	
+	public static void put_data_to_hdfs(int index, String outputData) throws IOException
+	{
+		Path p = new Path("hdfs://"+hostname+":9000/hduser/R/OGA/input/XData"+index+".csv");
+		byte[] byt = outputData.getBytes();
+		FSDataOutputStream fsOutStream = hdfs.create(p);
+		fsOutStream.write(byt);
+		fsOutStream.close();
+	}
+	
+	
     public static void main( String[] args ) throws IOException
     {
-    	String inputxPath = "inputx.csv";
-    	String inputyPath = "inputy.csv";
+   
+    	System.out.println("--------- Program Start -------");
     	
-    	// FILE Read
+    	String inputxPath = "C://Users/Jax/Desktop/inputx.csv";  // the X data input path
+    	String inputyPath = "C://Users/Jax/Desktop/inputy.csv"; // the Y data input path
+    	String inputBuffer = "";  
+    	String outputBuffer = "";
+    	int xSize = 0;
+   
+    	// set Hadoop Configuration
+    	
+    	// set_hadoop();
+    	
+    	// set FILE Reader
         FileReader xFr = new FileReader(inputxPath);
         FileReader yFr = new FileReader(inputyPath);
+        BufferedReader xBr = new BufferedReader(xFr);
+        BufferedReader yBr = new BufferedReader(yFr);
+
+        // first read
         
-        xFr.close();
+    	inputBuffer = xBr.readLine();
+    	String[] splitRow = inputBuffer.split(",");
+    	
+    	// get the X size
+   		xSize = splitRow.length;
+   		xBr.close();
+   		xFr.close();
+   		
+   		int i = 0;
+   		
+        for(i=0;i<xSize;i++){
+        	
+        	xFr = new FileReader(inputxPath);
+        	xBr = new BufferedReader(xFr);
+        	
+		    while(xBr.ready()){
+		    	
+		    	// split data
+	
+	    		inputBuffer = xBr.readLine();
+	    		splitRow = inputBuffer.split(",");
+
+		    	outputBuffer = outputBuffer + splitRow[i] + ",";
+		    	
+		    }
+		    
+		    outputBuffer = outputBuffer.substring(0, outputBuffer.length() - 1);
+		    
+		    // put data to hdfs
+		    put_data_to_hdfs(i, outputBuffer);
+		    
+		   // System.out.println(outputBuffer);
+		    
+		    // reset
+		    
+		    outputBuffer = "";
+		    xBr.close();
+		    xFr.close();
+		    
+        }
+        	
+       
         yFr.close();
         
     }
