bd53f823d108a00f2c0f88513b7040c344b6bf41
Jax <liugs963@gmail.cm>
2015-07-10 16:47:47 +0800

    first commit

---
+++ src/main/java/OGAInputLoader/OGAInputLoader/InputLoader.java
@@ -0,0 +1,29 @@
+package OGAInputLoader.OGAInputLoader;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class InputLoader {
+	
+    public static void main( String[] args )
+    {
+    	String inputxPath = "inputx.csv";
+    	String inputyPath = "inputy.csv";
+    	
+    	// FILE Read
+        FileReader xFr = new FileReader(inputxPath);
+        FileReader yFr = new FileReader()
+        
+        
+    }
+
+}
